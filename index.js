67aa111eca2afe4c823285e960f2f11b14683e79
Ricardo Cepeda Raza <ricardo.cepeda.raza@gmail.com>
2023-10-16 21:13:45 -0400

    Express routing example

--- index.js
+++ index.js
@@ -5,14 +5,29 @@ const path = require("path");
 
 // Sets up the public path
 const publicPath = path.resolve(__dirname, "public");
-console.log(__dirname);
-console.log(publicPath);
 // Sends static files from the publicPath directory
 app.use(express.static(publicPath));
 
+// Called when a request to the root is made
+app.get('/', function(req, res) {
+    res.end("Welcome to my homepage");
+})
+// Called when a request to /about is made
+app.get('/about', function(req, res) {
+    res.end("Welcom to the about page");
+})
+// Called when a request to /weather comes in
+app.get('/weather', function(req, res) {
+    res.end('The current weather is NICE')
+})
+// Grabbing data from routes
+app.get('/hello/:who', function(req, res) {
+    res.end(`Hello ${req.params.who}.`);
+})
+// If you miss the others, you'll end up here.
 app.use(function(req, res) {
-    res.writeHead(200, {"Content-type": "text/plain"});
-    res.end("Looks like you didn't find a static file.");
+    res.statusCode = 404;
+    res.end("404");
 })
 
 http.createServer(app).listen(3000);
